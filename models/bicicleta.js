503d9d9468cd6655f41290a4140f9fc298fc4273
aduii <alexjuep@hotmail.com>
2021-05-26 21:33:43 -0500

    CRUD Methods

---
+++ models/bicicleta.js
@@ -0,0 +1,43 @@
+var Bicicleta = function (id, color, modelo, ubicacion) {
+  this.id = id;
+  this.color = color;
+  this.modelo = modelo;
+  this.ubicacion = ubicacion;
+};
+
+Bicicleta.prototype.toString = () => {
+  `id:  ${this.id} | color: ${this.color}`;
+};
+
+Bicicleta.allBicis = [];
+
+Bicicleta.add = (aBici) => {
+  Bicicleta.allBicis.push(aBici);
+};
+
+Bicicleta.findById = (biciId) => {
+  var abici = Bicicleta.allBicis.find((x) => x.id == biciId);
+  if (abici) {
+    return abici;
+  } else {
+    throw new Error(`No existe una bicicleta con el id ${biciId}`);
+  }
+};
+
+Bicicleta.removeById = (biciId) => {
+  // var bici = Bicicleta.findById(biciId);
+  for (let i = 0; i < Bicicleta.allBicis.length; i++) {
+    if (Bicicleta.allBicis[i].id == biciId) {
+      Bicicleta.allBicis.splice(i, 1);
+      break;
+    }
+  }
+};
+
+var a = new Bicicleta(1, 'rojo', 'urbana', [-11.487432, -77.232531]);
+var b = new Bicicleta(2, 'blanca', 'urbana', [-11.487435, -77.232541]);
+
+Bicicleta.add(a);
+Bicicleta.add(b);
+
+module.exports = Bicicleta;
